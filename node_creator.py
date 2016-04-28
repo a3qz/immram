e50d6939745f194212e0dcd72753241ca8d3b58a
a3qz <rmichalec14@gmail.com>
2016-04-28 16:45:01 -0400

    A couple nodes down

--- node_creator.py
+++ node_creator.py
@@ -5,14 +5,17 @@ import json
 import os
 
 
-#/fp = open("./nodes.json", "wr")
+fp = open("./nodes.json", "r")
 
-#NODES = json.load(fp)
-more = True
+JOURNEY = json.load(fp)
+fp.close()
+more = "1"
 inner_dict = {}
 NODES = {}
-while(more):
+#JOURNEY = {}
+while(more != "0"):
     inner_dict.clear()
+    NODES.clear()
     node_name = raw_input("Please enter the name of this node:  \n")
     inner_dict_name_1 = raw_input("Please enter the name of the first room: \n")
     inner_dict_name_2 = raw_input("Please enter the name of the second room: \n")
@@ -20,12 +23,13 @@ while(more):
     inner_dict[inner_dict_name_1] = raw_input("Please enter the text of the first room:\n")
     inner_dict[inner_dict_name_2] = raw_input("Please enter the text of the second room:\n")
     
-    NODES["name"] = raw_input("Please enter the text of the main node: \n")
+    NODES["text"] = raw_input("Please enter the text of the main node: \n")
     NODES["branches"] = inner_dict
+    JOURNEY[node_name] = NODES
     
     with open('nodes.json.new', 'w') as stream:
-            json.dump(NODES, stream)
+            json.dump(JOURNEY, stream)
             os.rename('nodes.json.new', 'nodes.json')
-    more = raw_input ( "Enter 1 to enter another node, 0 to exit")
+    more = raw_input ( "Enter 1 to enter another node, 0 to exit: ")
 
 
