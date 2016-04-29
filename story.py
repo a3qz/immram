1eed1339dc4319cb401c6caecb96e9a814140e2d
Ryan Steven Michalec <rmichale@fitz72.helios.nd.edu>
2016-04-28 21:24:16 -0400

    11

--- story.py
+++ story.py
@@ -12,8 +12,12 @@ fp = open("./nodes.json", "r")
 JOURNEY = json.load(fp)
 fp.close()
 store = []
-num_islands = 3
-
+num_islands = 9999
+os.system('cls' if os.name == 'nt' else 'clear')
+sys.stdout.write("Welcome Traveler! You are about to embark on an epic quest.  You have recieved a prophecy that revaled to you great blessings will come if you return home safely from a sea voyage.  Therefore, you are going to set sail with a company of your closest and bravest companions, with whom you would trust your life.  " )
+sys.stdout.write('\n' +'\n')
+while not ((num_islands > 2 ) & (num_islands < 8)):
+	num_islands = input("Please enter the number of islands you would like to visit on this journey (3-7 please):  ")
 for i in range(num_islands):
 	os.system('cls' if os.name == 'nt' else 'clear')
 	store = []
@@ -41,6 +45,27 @@ for i in range(num_islands):
 		#sys.stdout.write('\n' +'\n' )
 		sys.stdout.write(  JOURNEY[story]["branches"][JOURNEY[story]["branches"].keys()[move-1]])
 		move = 9999
+
+
+
+
+switch = random.randint(0,4)
+if switch == 0:
+	sys.stdout.write("You have reached the end of your voyage, and have returned home safely!  You are welcomed back with a majestic feast, but slowly life returns to normal.  However, you can't shake the feeling that things just keep going your way these days.  " )
+elif switch == 1:
+	sys.stdout.write("You have managed to reach the end of your voyage without losing too much blood, congratulations.  As you walk back onto the shore, you are approached by a stranger you have never seen before.  He asks about your voyage, and you tell him your tale.  He listens with fascination, and when you complete the tale, he slips a small bag stuffed full of gold and precious gems into your hand and thanks you before walking away.  You sit there stunned, and before you realize it, he is out of your sight.  ")
+elif switch == 2:
+	sys.stdout.write("As your company approaches home, you are elated, as it seems like you are finally out of danger.  However, a chill soon runs over the crew as you catch site of home.  You see a little too much smoke in the air, and see no boats slightly offshore, as is usual.  As you land at the port, you see your entire village has been abandoned, and your town hall burned to the ground.  ")
+elif switch == 3:
+	sys.stdout.write("You reached home.  You leave the boat and celebrate, but everything feels hollow and different now.  After your time at sea, your life on the land feels monotonous, and you cannot escape the feeling of being trapped.  ")
+elif switch == 4:
+	sys.stdout.write("You reach home, happy to finally be back where you belong.  You go back to your normal life, find a spouse to happily marry, and have a large family that you are proud of.  ")
+sys.stdout.write('\n' +'\n')
+
+
+
+
+
 
 
 
